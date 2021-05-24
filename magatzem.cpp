5e0eb678139d2fce619936b82693c551ad3bccaa
mohabh15 <68891712+mohabh15@users.noreply.github.com>
2021-05-24 18:31:30 +0200

    update casi

--- magatzem.cpp
+++ magatzem.cpp
@@ -8,6 +8,8 @@ Magatzem::Magatzem()
 Magatzem::Magatzem(int mida_magatzem, vector<int>& llista_cambres)    
 {
     //algoritmo para modificar el vector con la estructura de preorden a post orden
+
+    
     magatzem=generar_arbre(mida_magatzem,llista_cambres);
     Cambra aux;
     while(mida_magatzem>0)
